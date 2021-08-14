e49cb6396541660f87ba43a5626d44e445a1350d
YxY <526924057@qq.com>
2021-08-14 23:22:08 +0800

    Map info

--- src/Server/Logic/SeamlessMapManager/SeamlessMapManager.go
+++ src/Server/Logic/SeamlessMapManager/SeamlessMapManager.go
@@ -4,17 +4,12 @@ import (
 	"YMsg"
 	"YNet"
 	ylog "YServer/Logic/Log"
-	module "YServer/Logic/Module"
 	user "YServer/Logic/User"
 	"github.com/yxinyi/YEventBus"
 	"time"
 )
 
 var G_seam_map_manager *SeamlessMapManager
-type SeamlessMapManager struct {
-	module.ModuleBase
-	M_user_manager map[uint64]*user.User
-}
 
 func New() *SeamlessMapManager {
 	G_seam_map_manager = &SeamlessMapManager{
@@ -30,17 +25,55 @@ func userOffline(session_ *YNet.Session) {
 
 }
 
+func GetMapIndexWithPosition(pos_ YMsg.PositionXY) SeamlessMapIndex {
+	return SeamlessMapIndex{}
+}
 
-func userEnterMap(session_ *YNet.Session) {
+func GetAOIArrWithPosition(pos_ YMsg.PositionXY, view_range_ float64) []SeamlessAOIIndex {
+	retArr := make([]SeamlessAOIIndex, 0)
+	return retArr
+}
+func GetMapUidWithAOI(SeamlessAOIIndex) uint32 {
+	return 0
+}
+func GetMapUidWithAOIArr(aoi_arr_ []SeamlessAOIIndex) []uint32 {
+	retArr := make([]uint32, 0)
+	for _, it := range aoi_arr_ {
+		retArr = append(retArr, GetMapUidWithAOI(it))
+	}
+
+	return retArr
+}
+
+func GetMapSession(map_uid_ uint32) *YNet.Session {
+	return nil
+}
+
+func CreateMapProcessWithCallBack(map_uid_ uint32, cb_ func()) *YNet.Session {
+	return nil
+}
+
+func userEnterMap(user_ *user.User) {
 	//判断当前玩家在哪个地图上,地图为固定大小故很容易算出
-
+	//_map_idx := GetMapIndexWithPosition(user_.M_pos)
 	//然后根据同步范围计算出当前玩家需要注册到哪些 AOI 监视列表中(故使用灯塔AOI方案),得到 AOI 列表
-
+	_aoi_arr := GetAOIArrWithPosition(user_.M_pos, user_.M_view_range)
 	//AOI 列表中,包含玩家坐标的AOI 为主AOI,玩家所有操作将转发至包含该AOI 的逻辑进程上,其他 AOI 则为拷贝数据,当主进程上的玩家数据变化后需要同步至其他进程
-
+	_sync_map_list := GetMapUidWithAOIArr(_aoi_arr)
 	//如果玩家初始坐标的map 还未开启,则新建一个 map 进程并与当前 manager 进行连接,连接完成后进行同步
+	for _, it := range _sync_map_list {
+		_map_sess := GetMapSession(it)
+		_enter_map := func() {
 
-	//后续的消息将先发送至本进程,再进行转发,转发至玩家真正所在的进程上
+		}
+		if _map_sess == nil {
+			CreateMapProcessWithCallBack(it, _enter_map)
+		} else {
+			_enter_map()
+		}
+	}
+
+	////后续的消息将先发送至本进程,再进行转发,转发至玩家真正所在的进程上
 }
 
 func mapCreate() {
@@ -50,42 +83,18 @@ func mapCreate() {
 
 	//创建 map 进程,并将本机地址传递,记录新进程的信息,建立对应GRPC通道
 
-	
 }
 
 func (b *SeamlessMapManager) Init() error {
 	YEventBus.Register("UserLogin", userLogin)
 	YEventBus.Register("UserOffline", userOffline)
 
-	YNet.Register(YMsg.MESSAGE_TEST,func(msg_ YMsg.Message,s_ YNet.Session){
+	YNet.Register(YMsg.MESSAGE_TEST, func(msg_ YMsg.Message, s_ YNet.Session) {
 		ylog.Info("MESSAGE_TEST [%v] ", msg_)
 	})
 	return nil
 }
 
-func (b *SeamlessMapManager)Update(time_ time.Time)  {
+func (b *SeamlessMapManager) Update(time_ time.Time) {
 	//ylog.Info("time [%v] ",time_.Unix())
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
